a125b6795118aab1949bd29018c9854c7d393052
Dan F-M <danfm@nyu.edu>
2013-04-25 13:36:08 -0400

    adding test data and test function

--- untrendy/__init__.py
+++ untrendy/__init__.py
@@ -6,3 +6,18 @@ from __future__ import absolute_import, unicode_literals
 __version__ = "0.0.1"
 
 from .untrendy import fit_trend, untrend, discontinuity_scalar, median
+
+
+def test():
+    from inspect import getmembers, isfunction
+    from . import tests
+
+    for o in getmembers(tests):
+        if isfunction(o[1]) and o[0].startswith("test"):
+            print("{0} ...".format(o[0]))
+            try:
+                o[1]()
+            except Exception as e:
+                print("Failed with: {0}".format(e))
+            else:
+                print("    Passed.")
